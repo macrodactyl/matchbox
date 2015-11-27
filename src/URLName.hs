c663b6cdd47210efe45673fa39a1a83bdc6ef609
Louis Pilfold <louis@lpil.uk>
2015-11-27 19:29:32 +0000

    Use Text rather than Strings

--- src/URLName.hs
+++ src/URLName.hs
@@ -2,13 +2,14 @@ module URLName
   ( convert
   ) where
 
-import Data.Char (isAlphaNum, isAscii, toLower)
+import Data.Char (isAlphaNum, isAscii)
+import Data.Text
 
-convert :: String -> String
-convert = map convertChar
+convert :: Text -> Text
+convert = Data.Text.map convertChar . toLower
 
 convertChar :: Char -> Char
 convertChar char
-  | isAscii char && isAlphaNum char = toLower char
+  | isAscii char && isAlphaNum char = char
   | char == '.' = '.'
   | otherwise   = '-'
--- test/URLNameSpec.hs
+++ test/URLNameSpec.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module URLNameSpec (main, spec) where
 
 import URLName
 import Test.Hspec
 import Test.QuickCheck
 import Data.Char (isLower, isAlpha, isDigit, isAscii)
+import Data.Text
 
 main :: IO ()
 main = hspec spec
@@ -21,10 +24,10 @@ spec =
       convert "1234567890" `shouldBe` "1234567890"
 
     it "converts punctuation" $
-      convert "hi!£$%^&*()-_" `shouldBe` "hi-----------" 
+      convert "hi!£$%^&*()-_" `shouldBe` "hi-----------"
 
     it "always results in alphanumerics and .s and -s" $ property $
-      \x -> all isOKChar $ convert (x :: String)
+      \x -> Data.Text.all isOKChar . convert . pack $ (x :: String)
 
 
 isOKChar :: Char -> Bool
